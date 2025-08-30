a7266c27eeac76f5b3216fa5ad9281d4e33caf49
Tomas <tomaslonnblad@gmail.com>
2025-08-30 18:43:48 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    // unoptimized: true,
-  },
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    });
+const nextConfig = {};
 
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+export default nextConfig;
